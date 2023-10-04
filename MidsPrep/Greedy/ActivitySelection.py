b40ea7e0749b90639d0914cc7c3a7913b96a4a06
Ramnaresh Ulaganathan <121196326+ramnareeesh@users.noreply.github.com>
2023-10-04 12:17:37 +0530

    exercise algorithms for activity selection. yes, it works and gives optimal solution

--- MidsPrep/Greedy/ActivitySelection.py
+++ MidsPrep/Greedy/ActivitySelection.py
@@ -17,6 +17,26 @@ def activity_selector(activities):
     return select
 
 
+# Suppose that instead of always selecting the first activity to finish, we instead select the last activity to
+# start that is compatible with all previously selected activities. Describe how this approach is a greedy
+# algorithm, and prove that it yields an optimal solution.
+
+def activity_selector_mod(activities):
+    select = []
+    activities.sort(key=sort_key)
+
+    k = len(activities) - 1
+    select.append(activities[k])
+    for i in range(len(activities) - 1, -1, -1):
+        if activities[i][1] <= activities[k][0]:
+            select.append(activities[i])
+            k = i
+
+    return select
+
+
 if __name__ == '__main__':
     list_acts = [(5, 9), (1, 2), (3, 4), (0, 6), (5, 7), (8, 9)]
     print("The selected activities are: ", activity_selector(list_acts))
+    print()
+    print("The selected activities are: ", activity_selector_mod(list_acts))
