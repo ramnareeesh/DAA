0ebe8de19fdf9ca6b162bfa1176665b83ca23a1e
Ramnaresh Ulaganathan <121196326+ramnareeesh@users.noreply.github.com>
2023-11-14 23:04:37 +0530

    01 knapsack

--- Dynamic Programming/01Knapsack Recursive.py
+++ Dynamic Programming/01Knapsack Recursive.py
@@ -1,10 +1,10 @@
 def Knapsack(weight, value, bag_size, item_number):
-    if item_number == 0 or bag_size == 0:
+    if item_number == 0 or bag_size == 0:  # base condition
         return 0
-    if weight[item_number-1] <= bag_size:
+    if weight[item_number-1] <= bag_size:  # recursive condition 1
         return max(value[item_number-1] + Knapsack(weight, value, bag_size-weight[item_number-1], item_number-1),
                    Knapsack(weight, value, bag_size, item_number-1))
-    elif weight[item_number-1] > bag_size:
+    elif weight[item_number-1] > bag_size:  # recursive condition 2
         return Knapsack(weight, value, bag_size, item_number-1)
 
 
