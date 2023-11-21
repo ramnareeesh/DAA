4940da62315d0d4c45e2374a51ed2731df77b325
Ramnaresh Ulaganathan <121196326+ramnareeesh@users.noreply.github.com>
2023-11-21 23:50:52 +0530

    lcs - recursive + dp(new) approaches

--- Dynamic Programming/LCS-recursion.py
+++ Dynamic Programming/LCS-recursion.py
@@ -11,7 +11,24 @@ def lcs(A, B, i, j):
         )
 
 
+def lcs_dp(A, B):
+    m = len(A)
+    n = len(B)
+
+    dp = [[0 for i in range(n+1)] for i in range(m+1)]
+
+    for i in range(1, m+1):
+        for j in range(1, n+1):
+            if A[i-1] == B[j-1]:
+                dp[i][j] = 1 + dp[i-1][j-1]
+            else:
+                dp[i][j] = max(dp[i][j-1],
+                               dp[i-1][j])
+    return dp, dp[m][n]
+
+
 if __name__ == '__main__':
     A = "bd"
     B = "abcd"
     print(lcs(A, B, 0, 0))
+    print(lcs_dp(A, B))
